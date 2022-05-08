37802a109dd9635e41bee2e6ef2f9fcf9fdd57a1
Hang Nguyen <thuhang.nguyen0101@gmail.com>
2022-05-08 15:55:15 -0700

    added a hamburger icon

---
+++ src/Components/Hamburger/Hamburger.jsx
@@ -0,0 +1,7 @@
+import './Hamburger.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+export default function Hamburger() {
+    return (<FontAwesomeIcon id='hamburger' icon={faBars} />)
+}
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
-import './Navbar.css'
+import './Navbar.css';
+import Hamburger from '../Hamburger/Hamburger.jsx';
 
 export default function Navbar() {
-    return (<h1>Navbar placeholder</h1>)
+    return (<Hamburger />)
 }
