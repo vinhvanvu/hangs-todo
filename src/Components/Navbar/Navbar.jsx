f4a5b77438a18f53f6efc7e614c2623531e5f69d
Hang Nguyen <thuhang.nguyen0101@gmail.com>
2022-05-08 16:36:04 -0700

    added title to the navbar and styled navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,13 @@
 import './Navbar.css';
 import Hamburger from '../Hamburger/Hamburger.jsx';
+import Tittle from '../tittle/tittle.jsx';
 
 export default function Navbar() {
-    return (<Hamburger />)
+    return (
+        <div className='navbar'>
+            <Hamburger />
+            <Tittle />
+            <div></div>
+        </div>
+        )
 }
---
+++ src/Components/tittle/tittle.jsx
@@ -0,0 +1,5 @@
+import './tittle.css';
+
+export default function Tittle() {
+    return (<h1 className='tittle'>Website todo</h1>)
+}
