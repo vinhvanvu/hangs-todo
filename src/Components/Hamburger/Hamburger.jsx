37f4b3891ec3d235d5b9a89bbae9fedec8fe0a0d
Hang Nguyen <thuhang.nguyen0101@gmail.com>
2022-05-11 14:58:07 -0700

    moved css import to the last in Hamburger.jsx

--- src/Components/Hamburger/Hamburger.jsx
+++ src/Components/Hamburger/Hamburger.jsx
@@ -1,6 +1,6 @@
-import './Hamburger.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import './Hamburger.css';
 
 export default function Hamburger() {
     return (<FontAwesomeIcon id='hamburger' icon={faBars} />)
