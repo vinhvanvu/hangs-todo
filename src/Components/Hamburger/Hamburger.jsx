32429b45129d2928670e059ea3e64d3e7fd89e21
Hang Nguyen <thuhang.nguyen0101@gmail.com>
2022-05-11 13:28:16 -0700

    added hamburger component

---
+++ src/Components/Hamburger/Hamburger.jsx
@@ -0,0 +1,7 @@
+import './Hamburger.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+export default function() {
+    return (<FontAwesomeIcon id='Hamburger' icon={faBars} />)
+}
