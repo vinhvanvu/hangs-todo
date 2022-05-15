1dc9ba1de590ce32c76287c974a56e1d9e300ed9
Hang Nguyen <thuhang.nguyen0101@gmail.com>
2022-05-15 14:07:18 -0700

    added a trashcan

--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -1,7 +1,10 @@
 import NewTaskButton from '../NewTaskButton/NewTaskButton.jsx';
 import Checkbox from '../Checkbox/Checkbox.jsx';
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import './TodoList.css';
+
 
 
 export default function TodoList() {
@@ -12,6 +15,7 @@ export default function TodoList() {
     return (
         <div className='todo-list'>
             <Checkbox onClick={handleChecked} checked={Checked}/>
+            <FontAwesomeIcon id='trashcan' icon={faTrashCan} />
             <NewTaskButton />
         </div>
     )
