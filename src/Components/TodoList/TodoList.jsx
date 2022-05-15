dd01b7327f334affe03114706ed2690f1d1fc44a
Hang Nguyen <thuhang.nguyen0101@gmail.com>
2022-05-15 13:46:09 -0700

    removed whitespaces

--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -4,13 +4,10 @@ import { useState } from 'react';
 import './TodoList.css';
 
 export default function TodoList() {
-    const [Checked, setChecked] = useState(false);
-    
-    const handleChecked = () => {
-        
+    const [Checked, setChecked] = useState(false);    
+    const handleChecked = () => {    
         setChecked(!Checked)
-    }
-    
+    }    
     return (
         <div className='todo-list'>
             <Checkbox onClick={handleChecked} checked={Checked}/>
