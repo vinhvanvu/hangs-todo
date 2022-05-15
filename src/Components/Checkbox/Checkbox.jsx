2f36a3a7be86ed2d5a9179dfc3b9872308e7ea29
Hang Nguyen <thuhang.nguyen0101@gmail.com>
2022-05-15 00:35:00 -0700

    added a checkbox

---
+++ src/Components/Checkbox/Checkbox.jsx
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-regular-svg-icons';
+import { faCab, faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import './Checkbox.css';
+
+export default function Checkbox(props) {
+    return (
+        <div onClick={props.onClick}>
+            {props.checked ? <FontAwesomeIcon icon={faCircleCheck} />:< FontAwesomeIcon icon={faCircle} />}
+        </div>
+    )
+}
--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -1,8 +1,20 @@
 import NewTaskButton from '../NewTaskButton/NewTaskButton.jsx';
+import Checkbox from '../Checkbox/Checkbox.jsx';
+import { useState } from 'react';
 import './TodoList.css';
 
 export default function TodoList() {
+    const [Checked, setChecked] = useState(false);
+    
+    const handleChecked = () => {
+        
+        setChecked(!Checked)
+    }
+    
     return (
-        <NewTaskButton />
+        <div>
+            <NewTaskButton />
+            <Checkbox onClick={handleChecked} checked={Checked}/>
+        </div>
     )
 }
